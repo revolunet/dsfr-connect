02689d9d9c07e698def5fa7b9da71eab1d583b0d
Thomas Ramé <thomas.rame@outlook.com>
2023-05-23 02:46:57 +0200

    aaaa

--- apps/docs/scripts/bootstrap-v5/build.ts
+++ apps/docs/scripts/bootstrap-v5/build.ts
@@ -1,32 +1,49 @@
 import fs from 'fs-extra';
 import handlebars from 'handlebars';
+import path from 'path';
 
 const framework = 'bootstrap-v5';
 
 // function download/build/run
 
 async function main() {
-  const srcFolderPath = 'src';
-  const templateFilePath = 'template.hbs';
-  const outputFilePath = 'output.txt';
+  const srcFolderPath = path.resolve(__dirname, '../../tmp/bootstrap-main/site/content/docs/5.3/examples');
+  const templateFilePath = path.resolve(__dirname, './template.stories.js');
+  const outputFolderPath = path.resolve(__dirname, `../../stories/frameworks/${framework}/`);
 
   try {
-    const files = await fs.readdir(srcFolderPath);
+    const entries = (await fs.readdir(srcFolderPath, { withFileTypes: true })).filter((entry) => !!entry);
 
-    const fileContents = await Promise.all(
-      files.map(async (fileName) => {
-        const filePath = `${srcFolderPath}/${fileName}`;
-        const fileContent = await fs.readFile(filePath, 'utf-8');
-        return { fileName, fileContent };
+    const storyTemplateContent = await fs.readFile(templateFilePath, 'utf-8');
+    const storyTemplate = handlebars.compile(storyTemplateContent);
+
+    await Promise.all(
+      entries.map(async (entry) => {
+        // Components are in directory, use it as the name
+        if (entry.isDirectory()) {
+          const componentName = entry.name;
+
+          const filePath = path.join(srcFolderPath, `${entry.name}/index.html`);
+          let componentHtml = await fs.readFile(filePath, 'utf-8');
+
+          // Remove meta content at the top that is visible when rendering the story and escape needed quotes for the template
+          componentHtml = componentHtml
+            .replace(/---[\s\S]*?---/, '')
+            .replace(/`/g, '\\`')
+            .replace(/\{\{[\s\S]*?\}\}/g, '') // Remove all templating pleceholders of the Bootstrap documentation
+            .trim();
+
+          // Format the story
+          const storyContent = storyTemplate({
+            framework: framework,
+            component: componentName,
+            html: componentHtml,
+          });
+
+          await fs.outputFile(path.join(outputFolderPath, componentName, 'index.stories.js'), storyContent);
+        }
       })
     );
-
-    const templateContent = await fs.readFile(templateFilePath, 'utf-8');
-    const template = handlebars.compile(templateContent);
-
-    const outputContent = template({ files: fileContents });
-
-    await fs.writeFile(outputFilePath, outputContent);
 
     console.log('successful operation');
   } catch (error) {
--- apps/docs/scripts/bootstrap-v5/template.stories.js
+++ apps/docs/scripts/bootstrap-v5/template.stories.js
@@ -1,7 +1,7 @@
 export default {
   title: '{{framework}}/{{component}}',
   render: ({ label, ...args }) => {
-    return `{{html}}`;
+    return `{{{html}}}`;
   },
 };
 

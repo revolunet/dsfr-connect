6805aa74ea191b01462a71d1b92c552950b6dccc
Thomas Ramé <thomas.rame@outlook.com>
2023-05-24 00:46:21 +0200

    not polluting

--- apps/docs/@types/global.d.ts
+++ apps/docs/@types/global.d.ts
@@ -2,3 +2,8 @@ declare module '*?raw' {
   const content: string;
   export default content;
 }
+
+declare module '*?inline' {
+  const content: string;
+  export default content;
+}
--- apps/docs/scripts/bootstrap-v5/template.stories.ts
+++ apps/docs/scripts/bootstrap-v5/template.stories.ts
@@ -1,9 +1,20 @@
 import { Meta, StoryFn } from '@storybook/html';
 
+import componentStyle from '@dsfrc/dsfr-connect/src/bootstrap-v5/index.scss?inline';
+
 import componentHtml from './index.html?raw';
 
 export default {
   title: '{{framework}}/{{component}}',
+  decorators: [
+    (story) => {
+      // Import the style here to not pollute other framework stories
+      const styleElement = document.createElement('style');
+      styleElement.textContent = componentStyle;
+
+      return `${styleElement.outerHTML}${story()}`;
+    },
+  ],
   render: ({ label, ...args }) => {
     return componentHtml;
   },

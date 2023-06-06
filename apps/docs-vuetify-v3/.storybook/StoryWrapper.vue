0beccbdff1a25afc4b36b2b4588c0112fcace476
Thomas Ramé <thomas.rame@outlook.com>
2023-06-06 17:36:29 +0200

    aaa

--- apps/docs-vuetify-v3/.storybook/StoryWrapper.vue
+++ apps/docs-vuetify-v3/.storybook/StoryWrapper.vue
@@ -1,29 +1,15 @@
 <template>
   <v-app :theme="themeName">
     <v-main>
-      <template v-if="!!$slots['story']">
-        <slot name="story"></slot>
-      </template>
+      <slot name="story"></slot>
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  mounted() {
-    console.log(5555555);
-    console.log(this.$slots);
-  },
   props: {
     themeName: String,
-  },
-  watch: {
-    themeName: function (newThemeName, oldThemeName) {
-      // Logique à exécuter lorsque props.themeName change
-      console.log('props.themeName a changé de', oldThemeName, 'à', newThemeName);
-
-      // Autres actions à entreprendre...
-    },
   },
 };
 </script>
